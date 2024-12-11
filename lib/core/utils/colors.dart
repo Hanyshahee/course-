0eeec8374795091b1be1d01dee279d8b5e91a32a
omar <flutteromar7@gmail.com>
2024-12-11 09:59:45 +0200

    add core

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+
+  static const Color primaryColor = Color(0xFF00C853);
+  static const Color primaryColorDark = Color(0xFF007E33);
+  static const Color primaryColorLight = Color(0xFF00E676);
+
+  static const Color secondaryColor = Color(0xFFC0CA33);
+  static const Color secondaryColorDark = Color(0xFF9E9D24);
+  static const Color secondaryColorLight = Color(0xFFE6E7A6);
+
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF9E9E9E);
+  static const Color lightGrey = Color(0xFFBDBDBD);
+  static const Color darkGrey = Color(0xFF4F4F4F);
+
+  static const Color red = Color(0xFFFF0000);
+  static const Color green = Color(0xFF00FF00);
+  static const Color blue = Color(0xFF0000FF);
+
+}
